e15beaa4a979ed819c4a0c9e0fb9ef8c423788b3
isabelbuap <isa_ave_2005@outlook.com>
2025-11-05 07:37:02 -0600

    Upload
    Firts cormmit

---
+++ data.js
@@ -0,0 +1,337 @@
+const DATA = [
+  {
+    "familia": "Opioides",
+    "farmaco": "Morfina",
+    "DosisUrlImage": "images/2.png",
+    "InformacionComplementariaUrlImage": "images/3.png"
+  },
+  {
+    "familia": "Opioides",
+    "farmaco": "Nalbufina",
+    "DosisUrlImage": "images/4.png",
+    "InformacionComplementariaUrlImage": "images/5.png"
+  },
+  {
+    "familia": "Opioides",
+    "farmaco": "Meperidina",
+    "DosisUrlImage": "images/6.png",
+    "InformacionComplementariaUrlImage": "images/7.png"
+  },
+  {
+    "familia": "Opioides",
+    "farmaco": "Metadona",
+    "DosisUrlImage": "images/8.png",
+    "InformacionComplementariaUrlImage": "images/9.png"
+  },
+  {
+    "familia": "Opioides",
+    "farmaco": "Pentazocina",
+    "DosisUrlImage": "images/10.png",
+    "InformacionComplementariaUrlImage": "images/11.png"
+  },
+  {
+    "familia": "Opioides",
+    "farmaco": "Buprenorfina",
+    "DosisUrlImage": "images/12.png",
+    "InformacionComplementariaUrlImage": "images/13.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Âcido acetilsalicílico",
+    "DosisUrlImage": "images/15.png",
+    "InformacionComplementariaUrlImage": "images/16.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Paracetamol",
+    "DosisUrlImage": "images/17.png",
+    "InformacionComplementariaUrlImage": "images/18.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Metamizol",
+    "DosisUrlImage": "images/19.png",
+    "InformacionComplementariaUrlImage": "images/20.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Naproxeno",
+    "DosisUrlImage": "images/21.png",
+    "InformacionComplementariaUrlImage": "images/22.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Ketoprofeno",
+    "DosisUrlImage": "images/23.png",
+    "InformacionComplementariaUrlImage": "images/24.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Diclofenaco",
+    "DosisUrlImage": "images/25.png",
+    "InformacionComplementariaUrlImage": "images/26.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Ketorolaco",
+    "DosisUrlImage": "images/27.png",
+    "InformacionComplementariaUrlImage": "images/28.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Ibuprofeno",
+    "DosisUrlImage": "images/29.png",
+    "InformacionComplementariaUrlImage": "images/30.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Indometacina",
+    "DosisUrlImage": "images/31.png",
+    "InformacionComplementariaUrlImage": "images/32.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Prixocam ",
+    "DosisUrlImage": "images/33.png",
+    "InformacionComplementariaUrlImage": "images/34.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Meloxicam",
+    "DosisUrlImage": "images/35.png",
+    "InformacionComplementariaUrlImage": "images/36.png"
+  },
+  {
+    "familia": "AINES",
+    "farmaco": "Celecoxib",
+    "DosisUrlImage": "images/37.png",
+    "InformacionComplementariaUrlImage": "images/38.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Hidrocortisona",
+    "DosisUrlImage": "images/40.png",
+    "InformacionComplementariaUrlImage": "images/41.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Dexametasona",
+    "DosisUrlImage": "images/42.png",
+    "InformacionComplementariaUrlImage": "images/43.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Betametasona",
+    "DosisUrlImage": "images/44.png",
+    "InformacionComplementariaUrlImage": "images/45.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Prednisona",
+    "DosisUrlImage": "images/46.png",
+    "InformacionComplementariaUrlImage": "images/47.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Metilprednisolona",
+    "DosisUrlImage": "images/48.png",
+    "InformacionComplementariaUrlImage": "images/49.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Triamcinolona",
+    "DosisUrlImage": "images/50.png",
+    "InformacionComplementariaUrlImage": "images/51.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Deflazacort",
+    "DosisUrlImage": "images/52.png",
+    "InformacionComplementariaUrlImage": "images/53.png"
+  },
+  {
+    "familia": "Corticoesteroides",
+    "farmaco": "Fludrocoftisona",
+    "DosisUrlImage": "images/54.png",
+    "InformacionComplementariaUrlImage": "images/55.png"
+  },
+  {
+    "familia": "Benzodiacepinas",
+    "farmaco": "Diazepam",
+    "DosisUrlImage": "images/57.png",
+    "InformacionComplementariaUrlImage": "images/58.png"
+  },
+  {
+    "familia": "Benzodiacepinas",
+    "farmaco": "Clordiazepóxido",
+    "DosisUrlImage": "images/59.png",
+    "InformacionComplementariaUrlImage": "images/60.png"
+  },
+  {
+    "familia": "Benzodiacepinas",
+    "farmaco": "Lorazepam",
+    "DosisUrlImage": "images/61.png",
+    "InformacionComplementariaUrlImage": "images/62.png"
+  },
+  {
+    "familia": "Benzodiacepinas",
+    "farmaco": "Oxacepan",
+    "DosisUrlImage": "images/63.png",
+    "InformacionComplementariaUrlImage": "images/64.png"
+  },
+  {
+    "familia": "Benzodiacepinas",
+    "farmaco": "Clonazepam",
+    "DosisUrlImage": "images/65.png",
+    "InformacionComplementariaUrlImage": "images/66.png"
+  },
+  {
+    "familia": "Benzodiacepinas",
+    "farmaco": "Triazolam",
+    "DosisUrlImage": "images/67.png",
+    "InformacionComplementariaUrlImage": "images/68.png"
+  },
+  {
+    "familia": "Barbitúricos",
+    "farmaco": "Fenobarbital",
+    "DosisUrlImage": "images/70.png",
+    "InformacionComplementariaUrlImage": "images/71.png"
+  },
+  {
+    "familia": "Barbitúricos",
+    "farmaco": "Pentobarbital",
+    "DosisUrlImage": "images/72.png",
+    "InformacionComplementariaUrlImage": "images/73.png"
+  },
+  {
+    "familia": "Barbitúricos",
+    "farmaco": "Tiopental",
+    "DosisUrlImage": "images/74.png",
+    "InformacionComplementariaUrlImage": "images/75.png"
+  },
+  {
+    "familia": "Barbitúricos",
+    "farmaco": "Primidona",
+    "DosisUrlImage": "images/76.png",
+    "InformacionComplementariaUrlImage": "images/77.png"
+  },
+  {
+    "familia": "Barbitúricos",
+    "farmaco": "Butabarbital",
+    "DosisUrlImage": "images/78.png",
+    "InformacionComplementariaUrlImage": "images/79.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Fenelzina",
+    "DosisUrlImage": "images/81.png",
+    "InformacionComplementariaUrlImage": "images/82.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Amitriptilina",
+    "DosisUrlImage": "images/83.png",
+    "InformacionComplementariaUrlImage": "images/84.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Imipramina",
+    "DosisUrlImage": "images/85.png",
+    "InformacionComplementariaUrlImage": "images/86.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Venlafaxina",
+    "DosisUrlImage": "images/87.png",
+    "InformacionComplementariaUrlImage": "images/88.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Amoxapina",
+    "DosisUrlImage": "images/89.png",
+    "InformacionComplementariaUrlImage": "images/90.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Trazadona",
+    "DosisUrlImage": "images/91.png",
+    "InformacionComplementariaUrlImage": "images/92.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Fluoxetina",
+    "DosisUrlImage": "images/93.png",
+    "InformacionComplementariaUrlImage": "images/94.png"
+  },
+  {
+    "familia": "Antidepresivos",
+    "farmaco": "Sertralina",
+    "DosisUrlImage": "images/95.png",
+    "InformacionComplementariaUrlImage": "images/96.png"
+  },
+  {
+    "familia": "Antipsicóticos",
+    "farmaco": "Clorpromazina",
+    "DosisUrlImage": "images/98.png",
+    "InformacionComplementariaUrlImage": "images/99.png"
+  },
+  {
+    "familia": "Antipsicóticos",
+    "farmaco": "Haloperidol",
+    "DosisUrlImage": "images/100.png",
+    "InformacionComplementariaUrlImage": "images/101.png"
+  },
+  {
+    "familia": "Anticonvulsivos",
+    "farmaco": "Carbamazepina",
+    "DosisUrlImage": "images/103.png",
+    "InformacionComplementariaUrlImage": "images/104.png"
+  },
+  {
+    "familia": "Anticonvulsivos",
+    "farmaco": "Fenitoína",
+    "DosisUrlImage": "images/105.png",
+    "InformacionComplementariaUrlImage": "images/106.png"
+  },
+  {
+    "familia": "Anticonvulsivos",
+    "farmaco": "Etosuximida",
+    "DosisUrlImage": "images/107.png",
+    "InformacionComplementariaUrlImage": "images/108.png"
+  },
+  {
+    "familia": "Anticonvulsivos",
+    "farmaco": "Ácido valproico",
+    "DosisUrlImage": "images/109.png",
+    "InformacionComplementariaUrlImage": "images/110.png"
+  },
+  {
+    "familia": "Anestésicos locales",
+    "farmaco": "Lidocaína",
+    "DosisUrlImage": "images/112.png",
+    "InformacionComplementariaUrlImage": "images/113.png"
+  },
+  {
+    "familia": "Anestésicos locales",
+    "farmaco": "Bupivacaína",
+    "DosisUrlImage": "images/114.png",
+    "InformacionComplementariaUrlImage": "images/115.png"
+  },
+  {
+    "familia": "Anestésicos locales",
+    "farmaco": "Mepivacaína",
+    "DosisUrlImage": "images/116.png",
+    "InformacionComplementariaUrlImage": "images/117.png"
+  },
+  {
+    "familia": "Anestésicos locales",
+    "farmaco": "Proparacaína",
+    "DosisUrlImage": "images/118.png",
+    "InformacionComplementariaUrlImage": "images/119.png"
+  },
+  {
+    "familia": "Antisépticos tópicos",
+    "farmaco": "Povidona iodada",
+    "DosisUrlImage": "images/121.png",
+    "InformacionComplementariaUrlImage": "images/122.png"
+  }];
