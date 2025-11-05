4b0b28d365faf5071ccb66d4d58e96f2df9cf88b
isabelbuap <isa_ave_2005@outlook.com>
2025-11-05 07:51:02 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -43,7 +43,7 @@ const DATA = [
   },
   {
     "familia": "AINES",
-    "farmaco": "Paracetamol",
+    "farmaco": "Paracetamol\t\t",
     "DosisUrlImage": "images/17.png",
     "InformacionComplementariaUrlImage": "images/18.png"
   },
@@ -334,4 +334,551 @@ const DATA = [
     "farmaco": "Povidona iodada",
     "DosisUrlImage": "images/121.png",
     "InformacionComplementariaUrlImage": "images/122.png"
-  }];
+  },
+  {
+    "familia": "Antisépticos tópicos",
+    "farmaco": "Hipoclorito de sodio",
+    "DosisUrlImage": "images/123.png",
+    "InformacionComplementariaUrlImage": "images/124.png"
+  },
+  {
+    "familia": "Antisépticos tópicos",
+    "farmaco": "Benzalconio y benzetonio",
+    "DosisUrlImage": "images/125.png",
+    "InformacionComplementariaUrlImage": "images/126.png"
+  },
+  {
+    "familia": "Antisépticos tópicos",
+    "farmaco": "Alcohol etílico",
+    "DosisUrlImage": "images/127.png",
+    "InformacionComplementariaUrlImage": "images/128.png"
+  },
+  {
+    "familia": "Antisépticos tópicos",
+    "farmaco": "Peróxido de hidrógeno",
+    "DosisUrlImage": "images/129.png",
+    "InformacionComplementariaUrlImage": "images/130.png"
+  },
+  {
+    "familia": "Antisépticos tópicos",
+    "farmaco": "Clorhexidina",
+    "DosisUrlImage": "images/131.png",
+    "InformacionComplementariaUrlImage": "images/132.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Nitrofurantoína",
+    "DosisUrlImage": "images/134.png",
+    "InformacionComplementariaUrlImage": "images/135.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Ácido nalidíxico",
+    "DosisUrlImage": "images/136.png",
+    "InformacionComplementariaUrlImage": "images/137.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Ácido uropipemídico",
+    "DosisUrlImage": "images/138.png",
+    "InformacionComplementariaUrlImage": "images/139.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Norfloxacino",
+    "DosisUrlImage": "images/140.png",
+    "InformacionComplementariaUrlImage": "images/141.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Ciprofloxacino",
+    "DosisUrlImage": "images/142.png",
+    "InformacionComplementariaUrlImage": "images/143.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Levofloxacino",
+    "DosisUrlImage": "images/144.png",
+    "InformacionComplementariaUrlImage": "images/145.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Gatifloxacino",
+    "DosisUrlImage": "images/146.png",
+    "InformacionComplementariaUrlImage": "images/147.png"
+  },
+  {
+    "familia": "Antisépticos orales",
+    "farmaco": "Moxifloxacino",
+    "DosisUrlImage": "images/148.png",
+    "InformacionComplementariaUrlImage": "images/149.png"
+  },
+  {
+    "familia": "Penicilinas",
+    "farmaco": "Penicilina benzatínica",
+    "DosisUrlImage": "images/151.png",
+    "InformacionComplementariaUrlImage": "images/152.png"
+  },
+  {
+    "familia": "Penicilinas",
+    "farmaco": "Penicilina procaínica",
+    "DosisUrlImage": "images/153.png",
+    "InformacionComplementariaUrlImage": "images/154.png"
+  },
+  {
+    "familia": "Penicilinas",
+    "farmaco": "Penicilina potásica ",
+    "DosisUrlImage": "images/155.png",
+    "InformacionComplementariaUrlImage": "images/156.png"
+  },
+  {
+    "familia": "Penicilinas",
+    "farmaco": "Amoxicilina",
+    "DosisUrlImage": "images/157.png",
+    "InformacionComplementariaUrlImage": "images/158.png"
+  },
+  {
+    "familia": "Penicilinas",
+    "farmaco": "Ampicilina",
+    "DosisUrlImage": "images/159.png",
+    "InformacionComplementariaUrlImage": "images/160.png"
+  },
+  {
+    "familia": "Penicilinas",
+    "farmaco": "Dicloxacilina",
+    "DosisUrlImage": "images/161.png",
+    "InformacionComplementariaUrlImage": "images/162.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Cefalexina",
+    "DosisUrlImage": "images/164.png",
+    "InformacionComplementariaUrlImage": "images/165.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Cefalotina",
+    "DosisUrlImage": "images/166.png",
+    "InformacionComplementariaUrlImage": "images/167.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Cefuroxima",
+    "DosisUrlImage": "images/168.png",
+    "InformacionComplementariaUrlImage": "images/169.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Ceftriaxona",
+    "DosisUrlImage": "images/170.png",
+    "InformacionComplementariaUrlImage": "images/171.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Cefixima",
+    "DosisUrlImage": "images/172.png",
+    "InformacionComplementariaUrlImage": "images/173.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Cefotaxima",
+    "DosisUrlImage": "images/174.png",
+    "InformacionComplementariaUrlImage": "images/175.png"
+  },
+  {
+    "familia": "Cefalosporinas",
+    "farmaco": "Cefepime",
+    "DosisUrlImage": "images/176.png",
+    "InformacionComplementariaUrlImage": "images/177.png"
+  },
+  {
+    "familia": "Macrólidos",
+    "farmaco": "Eritromicina",
+    "DosisUrlImage": "images/179.png",
+    "InformacionComplementariaUrlImage": "images/180.png"
+  },
+  {
+    "familia": "Macrólidos",
+    "farmaco": "Claritromicina",
+    "DosisUrlImage": "images/181.png",
+    "InformacionComplementariaUrlImage": "images/182.png"
+  },
+  {
+    "familia": "Macrólidos",
+    "farmaco": "Azitromicina",
+    "DosisUrlImage": "images/183.png",
+    "InformacionComplementariaUrlImage": "images/184.png"
+  },
+  {
+    "familia": "Tetraciclinas",
+    "farmaco": "Tetraciclina",
+    "DosisUrlImage": "images/186.png",
+    "InformacionComplementariaUrlImage": "images/187.png"
+  },
+  {
+    "familia": "Tetraciclinas",
+    "farmaco": "Oxitetraciclina",
+    "DosisUrlImage": "images/188.png",
+    "InformacionComplementariaUrlImage": "images/189.png"
+  },
+  {
+    "familia": "Tetraciclinas",
+    "farmaco": "Doxiciclina",
+    "DosisUrlImage": "images/190.png",
+    "InformacionComplementariaUrlImage": "images/191.png"
+  },
+  {
+    "familia": "Tetraciclinas",
+    "farmaco": "Minociclina",
+    "DosisUrlImage": "images/192.png",
+    "InformacionComplementariaUrlImage": "images/193.png"
+  },
+  {
+    "familia": "Aminoglucósidos",
+    "farmaco": "Gentamicina",
+    "DosisUrlImage": "images/195.png",
+    "InformacionComplementariaUrlImage": "images/196.png"
+  },
+  {
+    "familia": "Aminoglucósidos",
+    "farmaco": "Tobramicina",
+    "DosisUrlImage": "images/197.png",
+    "InformacionComplementariaUrlImage": "images/198.png"
+  },
+  {
+    "familia": "Aminoglucósidos",
+    "farmaco": "Amikacina",
+    "DosisUrlImage": "images/199.png",
+    "InformacionComplementariaUrlImage": "images/200.png"
+  },
+  {
+    "familia": "Aminoglucósidos",
+    "farmaco": "Netilmicina",
+    "DosisUrlImage": "images/201.png",
+    "InformacionComplementariaUrlImage": "images/202.png"
+  },
+  {
+    "familia": "Aminoglucósidos",
+    "farmaco": "Estreptomicina",
+    "DosisUrlImage": "images/203.png",
+    "InformacionComplementariaUrlImage": "images/204.png"
+  },
+  {
+    "familia": "Carbapenémicos",
+    "farmaco": "Imipenem",
+    "DosisUrlImage": "images/206.png",
+    "InformacionComplementariaUrlImage": "images/207.png"
+  },
+  {
+    "familia": "Carbapenémicos",
+    "farmaco": "Meropenem",
+    "DosisUrlImage": "images/208.png",
+    "InformacionComplementariaUrlImage": "images/209.png"
+  },
+  {
+    "familia": "Antibióticos diversos",
+    "farmaco": "Ácido clavulánico",
+    "DosisUrlImage": "images/211.png",
+    "InformacionComplementariaUrlImage": "images/212.png"
+  },
+  {
+    "familia": "Antibióticos diversos",
+    "farmaco": "Sulfametoxazol-Trimetropim",
+    "DosisUrlImage": "images/213.png",
+    "InformacionComplementariaUrlImage": "images/214.png"
+  },
+  {
+    "familia": "Antibióticos diversos",
+    "farmaco": "Lincomicina",
+    "DosisUrlImage": "images/215.png",
+    "InformacionComplementariaUrlImage": "images/216.png"
+  },
+  {
+    "familia": "Antibióticos diversos",
+    "farmaco": "Clindamicina",
+    "DosisUrlImage": "images/217.png",
+    "InformacionComplementariaUrlImage": "images/218.png"
+  },
+  {
+    "familia": "Antibióticos diversos",
+    "farmaco": "Cloranfenicol",
+    "DosisUrlImage": "images/219.png",
+    "InformacionComplementariaUrlImage": "images/220.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Nistatina ",
+    "DosisUrlImage": "images/222.png",
+    "InformacionComplementariaUrlImage": "images/223.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Tolnaftato",
+    "DosisUrlImage": "images/224.png",
+    "InformacionComplementariaUrlImage": "images/225.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Ketoconazol",
+    "DosisUrlImage": "images/226.png",
+    "InformacionComplementariaUrlImage": "images/227.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Miconazol",
+    "DosisUrlImage": "images/228.png",
+    "InformacionComplementariaUrlImage": "images/229.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Clotrimazol",
+    "DosisUrlImage": "images/230.png",
+    "InformacionComplementariaUrlImage": "images/231.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Fluconazol",
+    "DosisUrlImage": "images/232.png",
+    "InformacionComplementariaUrlImage": "images/233.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Anfotericina B",
+    "DosisUrlImage": "images/234.png",
+    "InformacionComplementariaUrlImage": "images/235.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Caspofungina",
+    "DosisUrlImage": "images/236.png",
+    "InformacionComplementariaUrlImage": "images/237.png"
+  },
+  {
+    "familia": "Antifúngicos",
+    "farmaco": "Micafungina",
+    "DosisUrlImage": "images/238.png",
+    "InformacionComplementariaUrlImage": "images/239.png"
+  },
+  {
+    "familia": "Antivirales",
+    "farmaco": "Aciclovir",
+    "DosisUrlImage": "images/241.png",
+    "InformacionComplementariaUrlImage": "images/242.png"
+  },
+  {
+    "familia": "Antivirales",
+    "farmaco": "Amantadina",
+    "DosisUrlImage": "images/243.png",
+    "InformacionComplementariaUrlImage": "images/244.png"
+  },
+  {
+    "familia": "Antivirales",
+    "farmaco": "Ribavirina",
+    "DosisUrlImage": "images/245.png",
+    "InformacionComplementariaUrlImage": "images/246.png"
+  },
+  {
+    "familia": "Antivirales",
+    "farmaco": "Zidovudina",
+    "DosisUrlImage": "images/247.png",
+    "InformacionComplementariaUrlImage": "images/248.png"
+  },
+  {
+    "familia": "Antiprotozoarios",
+    "farmaco": "Metronidazol",
+    "DosisUrlImage": "images/250.png",
+    "InformacionComplementariaUrlImage": "images/251.png"
+  },
+  {
+    "familia": "Antiprotozoarios",
+    "farmaco": "Diloxanida",
+    "DosisUrlImage": "images/252.png",
+    "InformacionComplementariaUrlImage": "images/253.png"
+  },
+  {
+    "familia": "Antiprotozoarios",
+    "farmaco": "Quinina",
+    "DosisUrlImage": "images/254.png",
+    "InformacionComplementariaUrlImage": "images/255.png"
+  },
+  {
+    "familia": "Antiprotozoarios",
+    "farmaco": "Primaquina",
+    "DosisUrlImage": "images/256.png",
+    "InformacionComplementariaUrlImage": "images/257.png"
+  },
+  {
+    "familia": "Antiprotozoarios",
+    "farmaco": "Cloroquina",
+    "DosisUrlImage": "images/258.png",
+    "InformacionComplementariaUrlImage": "images/259.png"
+  },
+  {
+    "familia": "Antihelmínticos",
+    "farmaco": "Albendazol",
+    "DosisUrlImage": "images/261.png",
+    "InformacionComplementariaUrlImage": "images/262.png"
+  },
+  {
+    "familia": "Antihelmínticos",
+    "farmaco": "Mebendazol",
+    "DosisUrlImage": "images/263.png",
+    "InformacionComplementariaUrlImage": "images/264.png"
+  },
+  {
+    "familia": "Antihelmínticos",
+    "farmaco": "Praziquantel",
+    "DosisUrlImage": "images/265.png",
+    "InformacionComplementariaUrlImage": "images/266.png"
+  },
+  {
+    "familia": "Antifímicos",
+    "farmaco": "Fase intensiva. Clave 2414. doTBal",
+    "DosisUrlImage": "images/268.png",
+    "InformacionComplementariaUrlImage": "images/269.png"
+  },
+  {
+    "familia": "Antifímicos",
+    "farmaco": "Fase de sostén. Clave 2415. DOTBAL-S",
+    "DosisUrlImage": "images/270.png",
+    "InformacionComplementariaUrlImage": "images/271.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Captobril ",
+    "DosisUrlImage": "images/273.png",
+    "InformacionComplementariaUrlImage": "images/274.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Losartán ",
+    "DosisUrlImage": "images/275.png",
+    "InformacionComplementariaUrlImage": "images/276.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Candesartán",
+    "DosisUrlImage": "images/277.png",
+    "InformacionComplementariaUrlImage": "images/278.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Metropolol",
+    "DosisUrlImage": "images/279.png",
+    "InformacionComplementariaUrlImage": "images/280.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Amlodipino",
+    "DosisUrlImage": "images/281.png",
+    "InformacionComplementariaUrlImage": "images/282.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Nifedipino",
+    "DosisUrlImage": "images/283.png",
+    "InformacionComplementariaUrlImage": "images/284.png"
+  },
+  {
+    "familia": "Antihipertensivos ",
+    "farmaco": "Verapamilo",
+    "DosisUrlImage": "images/285.png",
+    "InformacionComplementariaUrlImage": "images/286.png"
+  },
+  {
+    "familia": "Diuréticos",
+    "farmaco": "Hiroclorotiazida",
+    "DosisUrlImage": "images/288.png",
+    "InformacionComplementariaUrlImage": "images/289.png"
+  },
+  {
+    "familia": "Diuréticos",
+    "farmaco": "Clortalidona",
+    "DosisUrlImage": "images/290.png",
+    "InformacionComplementariaUrlImage": "images/291.png"
+  },
+  {
+    "familia": "Diuréticos",
+    "farmaco": "Furosemida",
+    "DosisUrlImage": "images/292.png",
+    "InformacionComplementariaUrlImage": "images/293.png"
+  },
+  {
+    "familia": "Diuréticos",
+    "farmaco": "Manitol",
+    "DosisUrlImage": "images/294.png",
+    "InformacionComplementariaUrlImage": "images/295.png"
+  },
+  {
+    "familia": "Diuréticos",
+    "farmaco": "Espironolactona",
+    "DosisUrlImage": "images/296.png",
+    "InformacionComplementariaUrlImage": "images/297.png"
+  },
+  {
+    "familia": "Hipoglucemiantes orales",
+    "farmaco": "Metformina",
+    "DosisUrlImage": "images/299.png",
+    "InformacionComplementariaUrlImage": "images/300.png"
+  },
+  {
+    "familia": "Hipoglucemiantes orales",
+    "farmaco": "Pioglitazona",
+    "DosisUrlImage": "images/301.png",
+    "InformacionComplementariaUrlImage": "images/302.png"
+  },
+  {
+    "familia": "Hipoglucemiantes orales",
+    "farmaco": "Glibenclamida",
+    "DosisUrlImage": "images/303.png",
+    "InformacionComplementariaUrlImage": "images/304.png"
+  },
+  {
+    "familia": "Hipoglucemiantes orales",
+    "farmaco": "Rosiglitazona",
+    "DosisUrlImage": "images/305.png",
+    "InformacionComplementariaUrlImage": "images/306.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Lispro",
+    "DosisUrlImage": "images/308.png",
+    "InformacionComplementariaUrlImage": "images/309.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Aspart",
+    "DosisUrlImage": "images/310.png",
+    "InformacionComplementariaUrlImage": "images/311.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Glulisina",
+    "DosisUrlImage": "images/312.png",
+    "InformacionComplementariaUrlImage": "images/313.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Acción rápida",
+    "DosisUrlImage": "images/314.png",
+    "InformacionComplementariaUrlImage": "images/315.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Acción intermedia",
+    "DosisUrlImage": "images/316.png",
+    "InformacionComplementariaUrlImage": "images/317.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Glargina",
+    "DosisUrlImage": "images/318.png",
+    "InformacionComplementariaUrlImage": "images/319.png"
+  },
+  {
+    "familia": "Insulinas",
+    "farmaco": "Detemir",
+    "DosisUrlImage": "images/320.png",
+    "InformacionComplementariaUrlImage": "images/321.png"
+  }
+];
